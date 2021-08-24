b5574f2b065d776ce1a236cdea7641e8eba2e758
Artem Basalaev <artem.basalaev@dualbootpartners.com>
2021-08-24 12:44:53 +0300

    First instance

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.38"
+    }
+  }
+
+  required_version = ">= 0.15.3"
+}
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+locals {
+  # FEDORA-COREOS
+  instance_ami = "ami-09e2e5104f310ffb5"
+}
+
+resource "aws_instance" "app_server" {
+  ami = local.instance_ami
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Study AppServer"
+  }
+}
